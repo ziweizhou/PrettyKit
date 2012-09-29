39d7d3c1c500b2459d369a47f036bc9faafc0638
Karim-Pierre Maalej <kpm@kypselia.com>
2012-09-29 21:44:36 +0200

    Storyboard example

---
+++ PrettyStoryboardExample/ExampleViewController.m
@@ -0,0 +1,163 @@
+//
+//  ExampleViewController.m
+//  PrettyExample
+//
+//  Created by Víctor on 29/02/12.
+//  Copyright (c) 2012 Victor Pena Placer. All rights reserved.
+//
+
+#import "ExampleViewController.h"
+#import "PrettyKit.h"
+#import "ModalViewController.h"
+
+
+@implementation ExampleViewController
+
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.tableView.backgroundView = nil; 
+    self.tableView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background"]];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 5;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    if (section == 0) {
+        return 3;
+    }
+    if (section == 1) {
+        return 1;
+    }
+    if (section == 2) {
+        return 1;
+    }
+    if (section == 3) {
+        return 20;
+    }
+    if (section == 4) {
+        return 1;
+    }
+    
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    static NSString *SegmentedCellIdentifier = @"SegmentedCell";
+    PrettySegmentedControlTableViewCell *segmentedCell;
+    static NSString *GridCellIdentifier = @"GridCell";
+    PrettyGridTableViewCell *gridCell;
+
+    
+    PrettyTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[PrettyTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.tableViewBackgroundColor = tableView.backgroundColor;        
+    }
+    
+    switch (indexPath.section) {
+        case 1:
+            switch (indexPath.row) {
+                case 0:
+                    segmentedCell = [tableView dequeueReusableCellWithIdentifier:SegmentedCellIdentifier];
+                    if (segmentedCell == nil) {
+                        segmentedCell = [[PrettySegmentedControlTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:SegmentedCellIdentifier];
+                    }
+                    [segmentedCell prepareForTableView:tableView indexPath:indexPath];
+                    segmentedCell.titles = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", nil];
+                    segmentedCell.tableViewBackgroundColor = tableView.backgroundColor;
+                    return segmentedCell;
+                default:
+                    break;
+            }
+
+            break;
+        case 2:
+            gridCell = [tableView dequeueReusableCellWithIdentifier:GridCellIdentifier];
+            if (gridCell == nil) {
+                gridCell = [[PrettyGridTableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:GridCellIdentifier];
+                gridCell.tableViewBackgroundColor = tableView.backgroundColor;
+                gridCell.actionBlock = ^(NSIndexPath *indexPath, int selectedIndex) {
+                    [gridCell deselectAnimated:YES];
+                };                
+            }
+            [gridCell prepareForTableView:tableView indexPath:indexPath];            
+            gridCell.numberOfElements = 3;
+            [gridCell setText:@"One" atIndex:0];
+            [gridCell setDetailText:@"Detail Text" atIndex:0];
+            [gridCell setText:@"Two" atIndex:1];            
+            [gridCell setDetailText:@"Detail Text" atIndex:1];
+            [gridCell setText:@"Three" atIndex:2];
+            [gridCell setDetailText:@"Detail Text" atIndex:2];            
+            return gridCell;
+        case 4:
+            gridCell = [tableView dequeueReusableCellWithIdentifier:GridCellIdentifier];
+            if (gridCell == nil) {
+                gridCell = [[PrettyGridTableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:GridCellIdentifier];
+                gridCell.tableViewBackgroundColor = tableView.backgroundColor;
+                gridCell.actionBlock = ^(NSIndexPath *indexPath, int selectedIndex) {
+                    [gridCell deselectAnimated:YES];
+                };                
+            }
+            [gridCell prepareForTableView:tableView indexPath:indexPath];            
+            gridCell.numberOfElements = 2;
+            [gridCell setText:@"Four" atIndex:0];
+            [gridCell setText:@"Five" atIndex:1];            
+            return gridCell;
+
+        default:
+            break;
+    }
+    
+    // Configure the cell...
+    [cell prepareForTableView:tableView indexPath:indexPath];
+    cell.textLabel.text = @"Text";
+    if (indexPath.section == 0) {
+        cell.cornerRadius = 20;
+    }
+    else {
+        cell.cornerRadius = 10;
+    }
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+-(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return tableView.rowHeight + [PrettyTableViewCell tableView:tableView neededHeightForIndexPath:indexPath];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
---
+++ PrettyStoryboardExample/PlainExample.m
@@ -0,0 +1,122 @@
+//
+//  PlainExample.m
+//  PrettyExample
+//
+//  Created by Víctor on 22/03/12.
+//  Copyright (c) 2012 Victor Pena Placer. All rights reserved.
+//
+
+#import "PlainExample.h"
+#import "PrettyKit.h"
+
+
+#define start_color [UIColor colorWithHex:0xEEEEEE]
+#define end_color [UIColor colorWithHex:0xDEDEDE]
+
+@implementation PlainExample
+
+- (void) customizeNavBar {
+    PrettyNavigationBar *navBar = (PrettyNavigationBar *)self.navigationController.navigationBar;
+    
+    navBar.topLineColor = [UIColor colorWithHex:0xFF1000];
+    navBar.gradientStartColor = [UIColor colorWithHex:0xDD0000];
+    navBar.gradientEndColor = [UIColor colorWithHex:0xAA0000];    
+    navBar.bottomLineColor = [UIColor colorWithHex:0x990000];   
+    navBar.tintColor = navBar.gradientEndColor;
+    navBar.roundedCornerRadius = 8;
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.navigationItem.title = @"Rounded PrettyKit NavBar";
+    self.tableView.rowHeight = 60;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.tableView.backgroundColor = [UIColor scrollViewTexturedBackgroundColor];
+
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose target:nil action:nil];
+
+    [self.tableView dropShadows];
+    [self customizeNavBar];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath  {
+    return tableView.rowHeight + [PrettyTableViewCell tableView:tableView neededHeightForIndexPath:indexPath];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (indexPath.row == 1) {
+        static NSString *GridCellIdentifier = @"GridCell";
+        
+        PrettyGridTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:GridCellIdentifier];
+        if (cell == nil) {
+            cell = [[PrettyGridTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:GridCellIdentifier];
+            cell.tableViewBackgroundColor = tableView.backgroundColor;
+            cell.gradientStartColor = start_color;
+            cell.gradientEndColor = end_color;  
+        }
+        cell.numberOfElements = 2;
+        [cell setActionBlock:^(NSIndexPath *indexPath, int selectedIndex) {
+            [cell deselectAnimated:YES];
+        }];
+        [cell prepareForTableView:tableView indexPath:indexPath];
+        cell.textLabel.font = [UIFont boldSystemFontOfSize:18];
+        [cell setText:@"Text 1" atIndex:0];
+        [cell setText:@"Text 2" atIndex:1];
+        [cell setDetailText:@"Subtitle" atIndex:0];
+        [cell setDetailText:@"Subtitle" atIndex:1];
+        
+        return cell;
+    }
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    PrettyTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[PrettyTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.tableViewBackgroundColor = tableView.backgroundColor;        
+        cell.gradientStartColor = start_color;
+        cell.gradientEndColor = end_color;  
+    }
+    [cell prepareForTableView:tableView indexPath:indexPath];
+    cell.textLabel.text = @"Text";
+    cell.textLabel.backgroundColor = [UIColor clearColor];
+    
+    
+    return cell;
+
+}
+
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
